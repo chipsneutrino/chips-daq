ce48cb7624fbe7fa8de9ccc0bd5ce23743f43113
Josh Tingey <j.tingey.16@ucl.ac.uk>
2019-08-21 21:06:56 -0400

    Implemented full power control for both replay board in the executable power

--- relay/src/power.cc
+++ relay/src/power.cc
@@ -1,3 +1,5 @@
+#include <boost/asio.hpp>
+
 #include <relay/relay.h>
 #include <relay/mc_relay.h>
 #include <relay/ec_relay.h>
@@ -7,21 +9,33 @@ int main(int argc, char *argv[])
     // Initialise the elasticsearch interface.
     g_elastic.init(true, false, 1); // log to stdout and use 1 threads for indexing
 
-    MCRelay mc_relay(3232238338); // Create a master container relay
-    mc_relay.status();
-    mc_relay.on(1);
-    sleep(1);
-    mc_relay.status();
-    mc_relay.off(1);
-    mc_relay.status();
+    if (argc != 5) {
+        std::cout << "Wrong number of arguments!" << std::endl;
+        return -1;
+    }
 
-    ECRelay ec_relay(3232238381); // Create a electronics container relay
-    ec_relay.status();
-    ec_relay.on(1);
-    sleep(1);
-    ec_relay.status();
-    ec_relay.off(1);
-    ec_relay.status();
+    boost::asio::ip::address_v4 address = boost::asio::ip::make_address_v4(argv[2]);
 
+    const std::string type{argv[1]};
+    std::unique_ptr<Relay> relay;
+    if(type == "mc") {
+        relay = std::unique_ptr<Relay>(new MCRelay(address.to_ulong()));
+    } else if (type == "ec") {
+        relay = std::unique_ptr<Relay>(new ECRelay(address.to_ulong()));
+    }
+
+    int channel = atoi(argv[3]);
+    int command = atoi(argv[4]);
+
+    if (command == 0) {
+        relay->off(channel);
+    } else if (command == 1) {
+        relay->on(channel);
+    } else {
+        std::cout << "Don't know that command!" << std::endl;
+        return -1;
+    }
+
+    relay->status();
     return 1;
 }
