2238968c60411dc73c14a9928907badfc17e08d2
Petr Manek <pmanek@fnal.gov>
2019-11-14 23:14:28 +0000

    integrate with threading library

--- spill_watchdogs/spill_forwarder_watchdog.py
+++ spill_watchdogs/spill_forwarder_watchdog.py
@@ -2,6 +2,7 @@
 
 import subprocess
 import time
+import threading
 from xmlrpc.server import SimpleXMLRPCServer
 
 # TODO: configure from environment
@@ -10,6 +11,11 @@ FORWARDER_HOLDOFF_SEC = 1
 
 LOOPBACK_HOSTNAME = 'localhost'
 LOOPBACK_PORT = 17898
+
+###
+
+g_observed_spill_times = {}
+g_observed_spill_times_lock = threading.Lock()
 
 def run_forwarder():
     running_forwarder = True
@@ -20,11 +26,12 @@ def run_forwarder():
 
         time.sleep(FORWARDER_HOLDOFF_SEC)
 
-def loopback_spill(nova_time, spill_type):
-    # TODO: keep track of last signal received grouped by type
-    return 'Ok'
-
 def run_loopback_receiver():
+    def loopback_spill(nova_time, spill_type):
+        with g_observed_spill_times_lock:
+            g_observed_spill_times[spill_type] = nova_time
+        return 'Ok'
+
     server = SimpleXMLRPCServer((LOOPBACK_HOSTNAME, LOOPBACK_PORT))
     server.register_function(loopback_spill, 'Spill')
     server.serve_forever()
@@ -39,8 +46,16 @@ def run_fsm_reporter():
     pass
 
 def main():
-    print "Hello watchdog!"
-    # TODO: run 3 threads with the methods above
+    threads = []
+    threads.append(threading.Thread(target=run_forwarder))
+    threads.append(threading.Thread(target=run_loopback_receiver))
+    threads.append(threading.Thread(target=run_fsm_reporter))
+
+    for thread in threads:
+        thread.start()
+        
+    for thread in threads:
+        thread.join()
 
 if __name__ == '__main__':
     main()
