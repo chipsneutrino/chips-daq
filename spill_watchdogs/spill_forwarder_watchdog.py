7524abe648dafb105374101680644dab5efed047
Petr Manek <pmanek@fnal.gov>
2019-11-20 01:06:41 +0000

    implement signal staleness monitoring

--- spill_watchdogs/spill_forwarder_watchdog.py
+++ spill_watchdogs/spill_forwarder_watchdog.py
@@ -14,12 +14,21 @@ FORWARDER_HOLDOFF_SEC = 1
 LOOPBACK_HOSTNAME = 'localhost'
 LOOPBACK_PORT = 17898
 
+SEC2NOVA = 64e6 # 1s contains 64M NOvA ticks
+FSM_REPORTER_INTERVAL = 1
+MONITORED_SIGNALS = {
+    0: 2 * SEC2NOVA,
+    3: 1 * SEC2NOVA
+}
+
 ###
 
+g_observed_spills = 0
 g_observed_spill_times = {}
 g_observed_spill_times_lock = threading.Lock()
 
 g_signalled = False
+g_stopping_event = threading.Event()
 g_stopping = False
 
 def signal_handler(sig_number, frame):
@@ -27,9 +36,10 @@ def signal_handler(sig_number, frame):
     global g_stopping
 
     if not g_signalled:
+        print('Got signal %d, shutting down gracefully. Signal again for immediate exit.' % sig_number)
         g_signalled = True
         g_stopping = True
-        print('Got signal %d, shutting down gracefully. Signal again to kill the process immediately.' % sig_number)
+        g_stopping_event.set()
     else:
         print('Signalled (%d) again, terminating hard.' % sig_number)
         sys.exit(0)
@@ -44,7 +54,11 @@ def run_forwarder():
         time.sleep(FORWARDER_HOLDOFF_SEC)
 
 class LoopbackDispatcher():
-     def _dispatch(self, method, params):
+    def _dispatch(self, method, params):
+        global g_observed_spill_times_lock
+        global g_observed_spill_times
+        global g_observed_spills
+
         if method != 'Spill':
             print('WARNING: Received unusual method (expected "Spill"): %s' % method)
             return
@@ -53,12 +67,11 @@ class LoopbackDispatcher():
             return
 
         nova_time, spill_type = params
-        if not isinstance(nova_time, int) or not isinstance(spill_type, int):
-            print('WARNING: Received unusual parameters (expected int, int): %s' % params)
-            return
         
+        g_observed_spills += 1
         with g_observed_spill_times_lock:
-            g_observed_spill_times[spill_type] = nova_time
+            g_observed_spill_times[int(spill_type)] = int(nova_time)
+
 
 def run_loopback_receiver():
     global g_stopping
@@ -74,19 +87,61 @@ def run_loopback_receiver():
     print('Loopback receiver done.')
 
 def run_fsm_reporter():
-    # TODO: run on timer
-    # TODO: check that sender PID on PPC is valid
-    # TODO: check that forwarder is running
-    # TODO: check that all loopback times are fresh
-    # TODO: send heartbeat through NNG to FSM
-    # TODO: if sender PID is not valid or (forwarder is running but times are not fresh), kill and restart sender
-    pass
+    global g_stopping
+    global g_stopping_event
+    global g_observed_spill_times_lock
+    global g_observed_spill_times
+    global g_observed_spills
+
+    local_spill_times = {}
+    local_spills = 0
+
+    stale_signals = {signal_type: -1 for signal_type in MONITORED_SIGNALS}
+
+    print('FSM reporter starting with monitoring interval %.f s.' % FSM_REPORTER_INTERVAL)
+    while not g_stopping:
+        new_local_spills = g_observed_spills
+        if new_local_spills > local_spills:
+            print('Received %d spill signals in the last monitoring period.' % (new_local_spills - local_spills))
+
+            with g_observed_spill_times_lock:
+                new_local_spill_times = g_observed_spill_times.copy()
+
+            stale_signals = {}
+            for signal_type in MONITORED_SIGNALS:
+                if signal_type not in new_local_spill_times:
+                    # signal was never seen before
+                    stale_signals[signal_type] = -1
+                    continue
+                elif signal_type not in local_spill_times:
+                    # signal has been seen only recently, don't have anything to compare against
+                    stale_signals[signal_type] = -2
+                    continue
+                
+                time_diff = new_local_spill_times[signal_type] - local_spill_times[signal_type]
+                if time_diff > MONITORED_SIGNALS[signal_type]:
+                    stale_signals[signal_type] = time_diff
+
+            local_spill_times = new_local_spill_times
+
+        # TODO: check that sender PID on PPC is valid
+        # TODO: check that forwarder is running
+        # TODO: send heartbeat through NNG to FSM
+        # TODO: if sender PID is not valid or (forwarder is running but times are not fresh), kill and restart sender
+        
+        if len(stale_signals) > 0:
+            print('WARNING: Found %d stale signals: %s' % (len(stale_signals), list(stale_signals.keys())))
+
+        local_spills = new_local_spills
+        g_stopping_event.wait(FSM_REPORTER_INTERVAL)
+
+    print('FSM reporter done.')
 
 def main():
     threads = []
     #threads.append(threading.Thread(target=run_forwarder))
     threads.append(threading.Thread(target=run_loopback_receiver))
-    #threads.append(threading.Thread(target=run_fsm_reporter))
+    threads.append(threading.Thread(target=run_fsm_reporter))
 
     for thread in threads:
         thread.start()
