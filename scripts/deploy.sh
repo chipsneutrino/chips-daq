86acc83b14d236d43d06c067b21fba22b7f70e81
Petr Manek <pmanek@fnal.gov>
2019-12-10 17:46:18 +0000

    add key doc

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -41,6 +41,8 @@ cp_scripts() {
 
 create_config() {
 	cp ./scripts/dist_config.sh ${BPATH}/config.sh
+
+	echo "# Base path of the deployment package" >>${BPATH}/config.sh
 	echo "export BPATH=\"${TGTPATH}/chips-dist\"" >>${BPATH}/config.sh
 }
 
