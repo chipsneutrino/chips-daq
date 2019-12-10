80adcbe14e2c15a85e1d27e5188b1248315ca38a
Petr Manek <pmanek@fnal.gov>
2019-12-10 22:42:05 +0000

    fix renamed config variable

--- tunnel/spill_tunnel.sh
+++ tunnel/spill_tunnel.sh
@@ -3,9 +3,9 @@
 set -e
 source ../config.sh
 
-K5REAUTH="${BPATH}/tunnel/k5reauth"
+K5REAUTH="${CHIPS_BASE_PATH}/tunnel/k5reauth"
 PRINCIPAL="pmanek@FNAL.GOV"
-KEYTAB="${BPATH}/tunnel/pmanek.keytab"
+KEYTAB="${CHIPS_BASE_PATH}/tunnel/pmanek.keytab"
 AUTOSSH="/usr/bin/autossh"
 REMOTE_PORT="17897"
 LOCAL_PORT="55812"
